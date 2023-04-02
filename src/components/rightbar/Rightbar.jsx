bf2dd30c52a612ea178a34856b200ed8a5cfb538
Harsh Gussain <99070976+Harsh-github-1@users.noreply.github.com>
2023-04-02 23:37:30 +0530

    Add files via upload

---
+++ src/components/rightbar/Rightbar.jsx
@@ -0,0 +1,86 @@
+import './rightbar.css'
+import { Users } from '../../dummyData'
+import Online from '../online/Online'
+
+export default function Rightbar({profile}) {
+
+  const HomeRightbar = () => {
+    return (
+      <>
+      <div className="birthdayContainer">
+            <img className='birthdayImg' src="assets/gift.png" alt="" />
+            <span className="birthdayText">
+              <b>Jeffrey Dahmer</b> and <b>2 other friends</b> have a birthday today.
+            </span>
+          </div>
+          <img className='rightbarAd' src="assets/ad.png" alt="" />
+          <h4 className="rightbarTitle">Online Friends</h4>
+          <ul className='rightbarFriendList'>
+            {Users.map((u) => (
+              <Online key={u.id} user={u} />
+            ))}
+          </ul>
+      </>
+    )
+  }
+
+  const ProfileRightBar = () =>{
+    return(
+      <>
+      <h4 className='rightbarTitle'>User Information</h4> 
+      <div className="rightbarInfo">
+        <div className="rightbarInfoItem">
+          <span className="rightbarInfoKey">City:</span>
+          <span className="rightbarInfoValue">New Delhi</span>
+        </div>
+        <div className="rightbarInfoItem">
+          <span className="rightbarInfoKey">From:</span>
+          <span className="rightbarInfoValue">Mumbai</span>
+        </div>
+        <div className="rightbarInfoItem">
+          <span className="rightbarInfoKey">Relationship:</span>
+          <span className="rightbarInfoValue">Single</span>
+        </div>
+      </div>
+      <h4 className='rightbarTitle'>User Friends</h4> 
+      <div className="rightbarFollowings">
+        <div className="rightbarFollowing">
+          <img src="assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">Lil Wayne</span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/2.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">Lil Wayne</span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/3.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">Lil Wayne</span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/4.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">Lil Wayne</span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/5.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">Lil Wayne</span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/6.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">Lil Wayne</span>
+        </div>
+      </div>
+
+      </>
+    )
+  }
+
+  return (
+    <>
+      <div className='rightbar'>
+        <div className="rightbarWrapper">
+          {profile ? <ProfileRightBar/> : <HomeRightbar/> }
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,24 @@
+import './register.css'
+
+export default function Register() {
+  return (
+    <div className='login'>
+        <div className="loginWrapper">
+            <div className="loginLeft">
+                <h3 className="loginLogo">The Social</h3>
+                <span className="loginDesc">Connect with anyone and everyone on your favourite social media app.</span>
+            </div>
+            <div className="loginRight">
+                <div className="loginBox">
+                    <input placeholder="Username" className="loginInput" />
+                    <input placeholder="Email" className="loginInput" />
+                    <input placeholder="Password" className="loginInput" />
+                    <input placeholder="Password Again" className="loginInput" />
+                    <button className="loginButton">Sign Up</button>
+                    <button className="loginRegister">Log into Account</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
